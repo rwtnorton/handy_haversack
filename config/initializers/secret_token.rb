d12ce9b363c98cd9d54de77afa50c326c9654c0c
Richard W. Norton <rwtnorton@gmail.com>
2011-05-06 00:37:17 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HandyHaversack::Application.config.secret_token = '59d59163145a0645d255c9283368ed7a7092048574e39cca907bdcc42ddbea03321db0aab91c18f89cc0b35181d712edb57e56114255f3c01f4f5bf86747f254'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HandyHaversack::Application.config.session_store :cookie_store, :key => '_handy_haversack_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HandyHaversack::Application.config.session_store :active_record_store
