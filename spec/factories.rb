a8315719ef74eec1a3c92c2461bcdac6d5c4cadc
Richard W. Norton <rwtnorton@gmail.com>
2011-05-14 19:32:25 -0400

    Added items to factory girl user.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,20 @@
 Factory.define :user do |user|
   user.email    'foo@example.com'
   user.password 'xyzzyfoobar'
+
+  user.after_build do |u|
+    u.items << Factory.build(:item, :user => u)
+  end
+
+  user.after_create do |u|
+    u.items.each do |item|
+      item.save!
+    end
+  end
+end
+
+Factory.define :item do |item|
+  item.name 'the_thing'
+  item.description 'interesting text, I am sure'
+  item.content 'Odi et amo quare id faciam fortasse requiris'
 end
