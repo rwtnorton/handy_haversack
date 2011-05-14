2dd9db53452bd8cb15be47905f352d36df8ea6c4
Richard W. Norton <rwtnorton@gmail.com>
2011-05-14 17:58:45 -0400

    Added items tests for un-signed-in users.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,54 +1,117 @@
 require 'spec_helper'
 
 describe ItemsController do
+  render_views
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
+  describe "not signed in" do
+
+    describe "GET 'new'" do
+      it "should deny access" do
+        get :new
+        response.should redirect_to user_session_path
+      end
+    end
+
+    describe "GET 'index'" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to user_session_path
+      end
+    end
+
+    describe "GET 'edit'" do
+      it "should deny access" do
+        get :edit
+        response.should redirect_to user_session_path
+      end
+    end
+
+    describe "GET 'show'" do
+      it "should deny access" do
+        get :show
+        response.should redirect_to user_session_path
+      end
+    end
+
+    describe "GET 'create'" do
+      it "should deny access" do
+        get :create
+        response.should redirect_to user_session_path
+      end
+    end
+
+    describe "GET 'update'" do
+      it "should deny access" do
+        get :update
+        response.should redirect_to user_session_path
+      end
+    end
+
+    describe "GET 'destroy'" do
+      it "should deny access" do
+        get :destroy
+        response.should redirect_to user_session_path
+      end
     end
   end
 
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
-  end
 
-  describe "GET 'edit'" do
-    it "should be successful" do
-      get 'edit'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "should be successful" do
-      get 'update'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destroy'" do
-    it "should be successful" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
+#  describe 'signed in' do
+#
+#    describe "GET 'new'" do
+#      it "should be successful" do
+#        get 'new'
+#        response.should be_success
+#      end
+#    end
+#
+#    describe "GET 'index'" do
+#      it "should be successful" do
+#        get 'index'
+#        response.should be_success
+#      end
+#
+#      it "should list items" do
+#        get 'index'
+#        response.should have_selector('ul#items')
+#      end
+#    end
+#
+#    describe "GET 'edit'" do
+#      it "should be successful" do
+#        get 'edit'
+#        response.should be_success
+#      end
+#    end
+#
+#    describe "GET 'show'" do
+#      it "should be successful" do
+#        get 'show'
+#        response.should be_success
+#      end
+#    end
+#
+#    describe "GET 'create'" do
+#      it "should be successful" do
+#        get 'create'
+#        response.should be_success
+#      end
+#    end
+#
+#    describe "GET 'update'" do
+#      it "should be successful" do
+#        get 'update'
+#        response.should be_success
+#      end
+#    end
+#
+#    describe "GET 'destroy'" do
+#      it "should be successful" do
+#        get 'destroy'
+#        response.should be_success
+#      end
+#    end
+#
+#  end
 
 end
